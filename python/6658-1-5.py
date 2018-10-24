05d4e9ceb6df0dcc3d60b02159d7f89745a7685b
Zeyu Chen <chenzy@gatech.edu>
2018-10-24 14:31:37 -0400

    Add Jupiter play

--- python/6658-1-5.py
+++ python/6658-1-5.py
@@ -23,7 +23,20 @@ _Mat = np.matrix([
                  [0, 0, 0, 0, 0, 0, 0.333333, 0],
                  ])
 
-H = (np.identity(8) - _Mat)
+H = (np.eye(8) - np.transpose(_Mat))
 H = H.I*np.ones((8, 1))
 
+print(np.transpose(_Mat))
 print(H)
+
+## 6
+
+P = np.matrix([
+              [0, 0.25, 0, 0.5],
+              [0.5, 0, 0.3333333, 0],
+              [0, 0.75, 0, 0.5],
+              [0.5, 0, 0.666666667, 0]
+              ])
+
+print(P*P*P*P*P*P*P*P*P*P*P*P*P*P*P*P*P*P)
+print(P*P*P*P*P*P*P*P*P*P*P*P*P*P*P*P*P*P*P)
---
+++ python/Lin20180912.py
@@ -0,0 +1,7 @@
+import numpy as np;
+
+print('Lab 1');
+name = input("What is your name?");
+print("Hi", name, ", welcome to the Python world.");
+input("Press Return to exit...");
+
